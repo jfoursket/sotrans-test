cc5ec0d46be089c904a4f3cbe088bf24b59409e3
jfoursket <jfoursket@gmail.com>
2024-12-11 15:07:42 +0300

    update nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const navItems = document.querySelectorAll(".nav-item");
 const menu = document.getElementById("menu");
 const burgerMenu = document.getElementById("burger-menu");
+const body = document.querySelector("body");
+const mobItems = document.querySelectorAll(".mob-item");
 
 navItems.forEach(item => {
     item.addEventListener("click", function () {
@@ -14,4 +16,19 @@ navItems.forEach(item => {
 
 burgerMenu.addEventListener("click", function () {
     menu.classList.toggle('menu-open');
+
+    if (menu.classList.contains('menu-open')) {
+        body.classList.add("overflow-hidden");
+    } else {
+        body.classList.remove("overflow-hidden");
+
+    }
 });
+
+mobItems.forEach(link => {
+    link.addEventListener("click", function () {
+        menu.classList.remove("menu-open");
+        body.classList.remove("overflow-hidden");
+        // navItems.forEach(i => i.classList.remove("open"));
+    })
+})
